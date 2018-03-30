698483c3c3324b7d11fe88e918daef5a6bf60107
crigalbla <cristian.gb.1995@hotmail.com>
2018-03-30 20:58:12 +0200

    Segundo commit

--- app/src/main/java/com/my/cristian/guiamiguelin/GoogleMaps.java
+++ app/src/main/java/com/my/cristian/guiamiguelin/GoogleMaps.java
@@ -1,18 +1,43 @@
 package com.my.cristian.guiamiguelin;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class GoogleMaps extends FragmentActivity implements OnMapReadyCallback {
 
+    private static int PETICION_PERMISO_LOCALIZACION = 101;
     private GoogleMap mMap;
+    private Marker marker;
+    double lat = 0.0;
+    double lng = 0.0;
+    String mensaje;
+    String street = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,26 +49,109 @@ public class GoogleMaps extends FragmentActivity implements OnMapReadyCallback {
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        myLocation();
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-//        LatLng myPoint = new LatLng(googleMap.getMyLocation().getLatitude(), googleMap.getMyLocation().getLongitude());
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+////        LatLng myPoint = new LatLng(googleMap.getMyLocation().getLatitude(), googleMap.getMyLocation().getLongitude());
+//
+////        mMap.addMarker(new MarkerOptions().position(myPoint).title("Mi posición"));
+////        mMap.moveCamera(CameraUpdateFactory.newLatLng(myPoint));
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16));
+    }
 
-//        mMap.addMarker(new MarkerOptions().position(myPoint).title("Mi posición"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(myPoint));
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    // Activar los servicios del gps cuando esten apagados
+    public void locationStart() {
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        final boolean gpsEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+        }
+    }
+
+    // Obtener la direccion de la calle a partir de la latitud y la longitud
+    public void setLocation(Location loc) {
+        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(), loc.getLongitude(), 1);
+                if (!list.isEmpty()) {
+                    Address DirCalle = list.get(0);
+                    street = (DirCalle.getAddressLine(0));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void agregateMarker(double lat, double lng) {
+        LatLng coordinates = new LatLng(lat, lng);
+        CameraUpdate myLocation = CameraUpdateFactory.newLatLngZoom(coordinates, 16);
+        if (marker != null) marker.remove();
+        marker = mMap.addMarker(new MarkerOptions()
+                .position(coordinates)
+                .title("Mi posición (Calle: " + street + ")")
+                .icon(BitmapDescriptorFactory.defaultMarker()));
+        mMap.animateCamera(myLocation);
+    }
+
+    private void uppdateUbication(Location location){
+        if (location != null) {
+            lat = location.getLatitude();
+            lng = location.getLongitude();
+            agregateMarker(lat, lng);
+        }
+    }
+
+    LocationListener locListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            uppdateUbication(location);
+            setLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+            mensaje = ("GPS Activado");
+            Mensaje();
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+            mensaje = ("GPS Desactivado");
+            locationStart();
+            Mensaje();
+        }
+    };
+
+    private void myLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PETICION_PERMISO_LOCALIZACION);
+            return;
+        } else {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            uppdateUbication(location);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,0,locListener);
+        }
+    }
+
+    public void Mensaje() {
+        Toast toast = Toast.makeText(this, mensaje, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
