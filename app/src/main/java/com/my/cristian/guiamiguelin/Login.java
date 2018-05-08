7a2abe959a1687467f4ca8d614bea7b1ee3d9e15
crigalbla <cristian.gb.1995@hotmail.com>
2018-05-08 10:27:12 +0200

    7º commit
    Commit por seguridad, para no perder nada

--- app/src/main/java/com/my/cristian/guiamiguelin/Login.java
+++ app/src/main/java/com/my/cristian/guiamiguelin/Login.java
@@ -2,7 +2,14 @@ package com.my.cristian.guiamiguelin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -10,8 +17,11 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
+    @BindView(R.id.toolbar_login)
+    Toolbar toolbar;
     @BindView(R.id.username)
     EditText username;
     @BindView(R.id.passaword)
@@ -22,9 +32,62 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.log_in);
+        setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        // Esto es el desplegable
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_login);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_login);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    @NonNull
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Intent i = null;
+
+        if (id == R.id.profile) {
+            i = new Intent(Login.this, Profile.class);
+        } else if (id == R.id.logout) {
+            // TODO
+        } else if (id == R.id.principal) {
+            i = new Intent(Login.this, MainActivity.class);
+        } else if (id == R.id.login) {
+            i = new Intent(Login.this, Login.class);
+        } else if (id == R.id.maps) {
+            i = new Intent(Login.this, GoogleMaps.class);
+        } else if (id == R.id.search_user) {
+            i = new Intent(Login.this, UserSearch.class);
+        } else if (id == R.id.search_estab) {
+            i = new Intent(Login.this, EstablishmentSearch.class);
+        }
+        if (i != null)
+            startActivity(i);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout_login);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    // Botones -------------------------------------------------------------------------------------
 
     @OnClick(R.id.log_in)
     public void loginOnClick() {
--- app/src/main/java/com/my/cristian/guiamiguelin/MainActivity.java
+++ app/src/main/java/com/my/cristian/guiamiguelin/MainActivity.java
@@ -3,6 +3,7 @@ package com.my.cristian.guiamiguelin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,7 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
@@ -43,7 +43,15 @@ public class MainActivity extends AppCompatActivity
 
         generateRestaurant();
 
+        // Todoo esto es para el desplegable
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+        // Para modificar el include de cada drawer
+        String strIncludeId = "include";
+        int includeId = getResources().getIdentifier(strIncludeId,"id", getPackageName());
+        CoordinatorLayout texto = (CoordinatorLayout) drawer.findViewById(includeId);
+
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -51,6 +59,84 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    // Esto es para cuando se pulse en navigation drawer (para desplegarlo)
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    // Menú de los 3 puntitos
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
+    // Deplegable de los 3 puntitos
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        // Esto son los 3 puntitos desplegables del menú
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    @NonNull
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Intent i = null;
+
+        if (id == R.id.profile) {
+            i = new Intent(MainActivity.this, Profile.class);
+        } else if (id == R.id.logout) {
+            // TODO
+        } else if (id == R.id.principal) {
+            i = new Intent(MainActivity.this, MainActivity.class);
+        } else if (id == R.id.login) {
+            i = new Intent(MainActivity.this, Login.class);
+        } else if (id == R.id.maps) {
+            i = new Intent(MainActivity.this, GoogleMaps.class);
+        } else if (id == R.id.search_user) {
+            i = new Intent(MainActivity.this, UserSearch.class);
+        } else if (id == R.id.search_estab) {
+            i = new Intent(MainActivity.this, EstablishmentSearch.class);
+        }
+        if (i != null)
+            startActivity(i);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    // Métodos auxiliares --------------------------------------------------------------------------
+
+    @Override
+    public void onItemClick(Restaurante restaurante) {
+
+    }
+
+    @Override
+    public void onLongItemClick(Restaurante restaurante) {
+
     }
 
     private void configToobar() {
@@ -84,78 +170,7 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        // Esto son los 3 puntitos desplegables del menú
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    @NonNull
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        Intent i = null;
-
-        if (id == R.id.profile) {
-            i = new Intent(MainActivity.this, Profile.class);
-        } else if (id == R.id.logout) {
-            // TODO
-        } else if (id == R.id.principal) {
-            i = new Intent(MainActivity.this, MainActivity.class);
-        } else if (id == R.id.login) {
-            i = new Intent(MainActivity.this, Login.class);
-        } else if (id == R.id.maps) {
-            i = new Intent(MainActivity.this, GoogleMaps.class);
-        } else if (id == R.id.search_user) {
-            i = new Intent(MainActivity.this, UserSearch.class);
-        } else if (id == R.id.search_estab) {
-            i = new Intent(MainActivity.this, EstablishmentSearch.class);
-        }
-        if (i != null)
-            startActivity(i);
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public void onItemClick(Restaurante restaurante) {
-
-    }
-
-    @Override
-    public void onLongItemClick(Restaurante restaurante) {
-
-    }
+    // Botones -------------------------------------------------------------------------------------
 
     @OnClick(R.id.maps)
     public void goToMaps() {
